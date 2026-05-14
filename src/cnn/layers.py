e64ae875990e6947fa4aa7219f4bb0832e9e3d26
Matthew12-t <18223096@std.stei.itb.ac.id>
2026-05-14 14:19:49 +0700

    feat: implement backward propagation

--- src/cnn/layers.py
+++ src/cnn/layers.py
@@ -10,10 +10,8 @@ def softmax(x):
     e = np.exp(x)
     return e / np.sum(e, axis=-1, keepdims=True)
 
-
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-np.clip(x, -500, 500)))
-
 
 _ACTIVATIONS = {
     "relu": relu,
@@ -22,7 +20,6 @@ _ACTIVATIONS = {
     "tanh": np.tanh,
     "linear": lambda x: x,
 }
-
 
 def get_activation(name):
     if name is None or name == "linear":
@@ -41,24 +38,51 @@ def _same_pad(H, W, kH, kW, stride_h, stride_w):
     pad_w = max((out_w - 1) * stride_w + kW - W, 0)
     return pad_h // 2, pad_h - pad_h // 2, pad_w // 2, pad_w - pad_w // 2
 
+def _col2im(dcols, N, H_pad, W_pad, C, kH, kW, stride_h, stride_w, out_h, out_w):
+    dx_pad = np.zeros((N, H_pad, W_pad, C), dtype=np.float32)
+    dcols_5d = dcols.reshape(N, out_h, out_w, kH, kW, C)
+    for i in range(out_h):
+        for j in range(out_w):
+            h0, w0 = i * stride_h, j * stride_w
+            dx_pad[:, h0:h0+kH, w0:w0+kW, :] += dcols_5d[:, i, j, :, :, :]
+    return dx_pad
+
+def _act_backward(dout, act_name, cache_z, cache_out):
+    if act_name is None or act_name == 'linear':
+        return dout
+    if act_name == 'relu':
+        return dout * (cache_z > 0)
+    if act_name == 'softmax':
+        s = cache_out
+        return s * (dout - (dout * s).sum(axis=-1, keepdims=True))
+    if act_name == 'sigmoid':
+        s = cache_out
+        return dout * s * (1 - s)
+    if act_name == 'tanh':
+        return dout * (1 - cache_out ** 2)
+    return dout
+
 
 # Layer Implementations
 class Conv2DLayer:
     def __init__(self, kernel, bias, strides=(1, 1), padding="valid", activation=None):
         self.kernel = np.asarray(kernel, dtype=np.float32)
-        self.bias   = np.asarray(bias,   dtype=np.float32)
+        self.bias = np.asarray(bias,   dtype=np.float32)
         self.stride_h, self.stride_w = strides
-        self.padding   = padding.lower()
+        self.padding = padding.lower()
+        self._act_name = activation
         self.activation = get_activation(activation)
         self.kH, self.kW = self.kernel.shape[:2]
+        self._cache = {}
 
     def forward(self, x):
         x = np.ascontiguousarray(x, dtype=np.float32)
         single = x.ndim == 3
         if single:
-            x = x[np.newaxis] # (H,W,C) menjadi (1,H,W,C)
+            x = x[np.newaxis]
 
         N, H, W, C = x.shape
+        pt = pb = pl = pr = 0
 
         if self.padding == "same":
             pt, pb, pl, pr = _same_pad(H, W, self.kH, self.kW, self.stride_h, self.stride_w)
@@ -79,24 +103,63 @@ class Conv2DLayer:
             x.strides[3],
         )
         patches = np.lib.stride_tricks.as_strided(x, shape=patch_shape, strides=patch_strides)
+        cols = patches.reshape(N, out_h * out_w, -1)
+        W_flat = self.kernel.reshape(-1, C_out)
+        z = (cols @ W_flat + self.bias).reshape(N, out_h, out_w, C_out)
+        out = self.activation(z) if self.activation else z
 
-        cols = patches.reshape(N, out_h * out_w, -1) # (N, out_h*out_w, kH*kW*C)
-        W_flat = self.kernel.reshape(-1, C_out) # (kH*kW*C, C_out)
-        out = (cols @ W_flat + self.bias).reshape(N, out_h, out_w, C_out)
-
-        out = self.activation(out) if self.activation else out
+        self._cache = {
+            'x_pad': x, 'cols': cols, 'z': z, 'out': out,
+            'N': N, 'H': H, 'W': W, 'C': C,
+            'out_h': out_h, 'out_w': out_w,
+            'pt': pt, 'pb': pb, 'pl': pl, 'pr': pr, 'single': single,
+        }
         return out[0] if single else out
+
+    def backward(self, dout):
+        c = self._cache
+        single = c['single']
+        if single:
+            dout = dout[np.newaxis]
+
+        N, out_h, out_w = c['N'], c['out_h'], c['out_w']
+        C_out = self.kernel.shape[-1]
+        C_in = c['C']
+
+        dz = _act_backward(dout, self._act_name, c['z'], c['out'])
+        dz_2d = dz.reshape(N * out_h * out_w, C_out)
+        cols_2d = c['cols'].reshape(N * out_h * out_w, -1)
+
+        db = dz.sum(axis=(0, 1, 2))
+        dW = (cols_2d.T @ dz_2d).reshape(self.kernel.shape)
+        dcols = (dz_2d @ self.kernel.reshape(-1, C_out).T).reshape(N, out_h * out_w, -1)
+
+        dx_pad = _col2im(dcols, N, c['H'], c['W'], C_in,
+                         self.kH, self.kW, self.stride_h, self.stride_w,
+                         out_h, out_w)
+
+        pt, pb, pl, pr = c['pt'], c['pb'], c['pl'], c['pr']
+        if pt or pb or pl or pr:
+            H_orig = c['H'] - pt - pb
+            W_orig = c['W'] - pl - pr
+            dx = dx_pad[:, pt:pt+H_orig, pl:pl+W_orig, :]
+        else:
+            dx = dx_pad
+
+        return (dx[0] if single else dx), dW, db
 
 
 class LocallyConnected2DLayer:
     def __init__(self, kernel, bias, kernel_size, strides=(1, 1), padding="valid", activation=None):
         self.kernel = np.asarray(kernel, dtype=np.float32)
-        self.bias   = np.asarray(bias,   dtype=np.float32)
+        self.bias = np.asarray(bias,   dtype=np.float32)
         ks = kernel_size if isinstance(kernel_size, (tuple, list)) else (kernel_size, kernel_size)
         self.kH, self.kW = ks
         self.stride_h, self.stride_w = strides
-        self.padding    = padding.lower()
+        self.padding = padding.lower()
+        self._act_name = activation
         self.activation = get_activation(activation)
+        self._cache = {}
 
     def forward(self, x):
         x = np.ascontiguousarray(x, dtype=np.float32)
@@ -115,21 +178,54 @@ class LocallyConnected2DLayer:
         out_h = (H - self.kH) // sh + 1
         out_w = (W - self.kW) // sw + 1
         C_out = self.kernel.shape[-1]
-        out = np.zeros((N, out_h, out_w, C_out), dtype=np.float32)
+        z = np.zeros((N, out_h, out_w, C_out), dtype=np.float32)
 
         for i in range(out_h):
             for j in range(out_w):
-                patch = x[:, i*sh:i*sh+self.kH, j*sw:j*sw+self.kW, :]  # (N,kH,kW,C)
-                out[:, i, j] = patch.reshape(N, -1) @ self.kernel[i, j] + self.bias[i, j]
+                patch = x[:, i*sh:i*sh+self.kH, j*sw:j*sw+self.kW, :]
+                z[:, i, j] = patch.reshape(N, -1) @ self.kernel[i, j] + self.bias[i, j]
 
-        out = self.activation(out) if self.activation else out
+        out = self.activation(z) if self.activation else z
+        self._cache = {'x_pad': x, 'z': z, 'out': out, 'N': N, 'H': H, 'W': W, 'C': C,
+                       'out_h': out_h, 'out_w': out_w, 'single': single}
         return out[0] if single else out
+
+    def backward(self, dout):
+        c = self._cache
+        single = c['single']
+        if single:
+            dout = dout[np.newaxis]
+
+        N, out_h, out_w = c['N'], c['out_h'], c['out_w']
+        sh, sw = self.stride_h, self.stride_w
+        x_pad = c['x_pad']
+        C_in = c['C']
+
+        dz = _act_backward(dout, self._act_name, c['z'], c['out'])
+        dW = np.zeros_like(self.kernel)
+        db = np.zeros_like(self.bias)
+        dx_pad = np.zeros_like(x_pad)
+
+        for i in range(out_h):
+            for j in range(out_w):
+                patch = x_pad[:, i*sh:i*sh+self.kH, j*sw:j*sw+self.kW, :]
+                patch_flat = patch.reshape(N, -1) # (N, kH*kW*C_in)
+                dz_ij = dz[:, i, j, :] # (N, C_out)
+
+                dW[i, j] = patch_flat.T @ dz_ij # (kH*kW*C_in, C_out)
+                db[i, j] = dz_ij.sum(axis=0)
+
+                dpatch = (dz_ij @ self.kernel[i, j].T).reshape(N, self.kH, self.kW, C_in)
+                dx_pad[:, i*sh:i*sh+self.kH, j*sw:j*sw+self.kW, :] += dpatch
+
+        return (dx_pad[0] if single else dx_pad), dW, db
 
 
 class MaxPooling2DLayer:
     def __init__(self, pool_size=(2, 2), strides=None):
         self.pH, self.pW = pool_size
         self.stride_h, self.stride_w = strides if strides else pool_size
+        self._cache = {}
 
     def forward(self, x):
         x = np.ascontiguousarray(x, dtype=np.float32)
@@ -151,14 +247,39 @@ class MaxPooling2DLayer:
             x.strides[3],
         )
         windows = np.lib.stride_tricks.as_strided(x, shape=win_shape, strides=win_strides)
-        out = windows.max(axis=(3, 4)) # (N, out_h, out_w, C)
+        out = windows.max(axis=(3, 4))
+
+        self._cache = {'x': x, 'out_h': out_h, 'out_w': out_w, 'single': single}
         return out[0] if single else out
+
+    def backward(self, dout):
+        c = self._cache
+        single = c['single']
+        if single:
+            dout = dout[np.newaxis]
+
+        x = c['x']
+        out_h = c['out_h']
+        out_w = c['out_w']
+        dx = np.zeros_like(x)
+
+        for i in range(out_h):
+            for j in range(out_w):
+                h0, w0 = i * self.stride_h, j * self.stride_w
+                window = x[:, h0:h0+self.pH, w0:w0+self.pW, :]
+                max_vals = window.max(axis=(1, 2), keepdims=True)
+                mask = (window == max_vals).astype(np.float32)
+                mask /= mask.sum(axis=(1, 2), keepdims=True)
+                dx[:, h0:h0+self.pH, w0:w0+self.pW, :] += mask * dout[:, i:i+1, j:j+1, :]
+
+        return dx[0] if single else dx
 
 
 class AvgPooling2DLayer:
     def __init__(self, pool_size=(2, 2), strides=None):
         self.pH, self.pW = pool_size
         self.stride_h, self.stride_w = strides if strides else pool_size
+        self._cache = {}
 
     def forward(self, x):
         x = np.ascontiguousarray(x, dtype=np.float32)
@@ -180,40 +301,123 @@ class AvgPooling2DLayer:
             x.strides[3],
         )
         windows = np.lib.stride_tricks.as_strided(x, shape=win_shape, strides=win_strides)
-        out = windows.mean(axis=(3, 4)) # (N, out_h, out_w, C)
+        out = windows.mean(axis=(3, 4))
+
+        self._cache = {'out_h': out_h, 'out_w': out_w, 'x_shape': x.shape, 'single': single}
         return out[0] if single else out
+
+    def backward(self, dout):
+        c = self._cache
+        single = c['single']
+        if single:
+            dout = dout[np.newaxis]
+
+        N, H, W, C = c['x_shape']
+        out_h, out_w = c['out_h'], c['out_w']
+        dx = np.zeros((N, H, W, C), dtype=np.float32)
+        scale = 1.0 / (self.pH * self.pW)
+
+        for i in range(out_h):
+            for j in range(out_w):
+                h0, w0 = i * self.stride_h, j * self.stride_w
+                dx[:, h0:h0+self.pH, w0:w0+self.pW, :] += scale * dout[:, i:i+1, j:j+1, :]
+
+        return dx[0] if single else dx
 
 
 class GlobalMaxPooling2DLayer:
+    def __init__(self):
+        self._cache = {}
+
     def forward(self, x):
+        self._cache = {'x': np.asarray(x, dtype=np.float32)}
         if x.ndim == 3:
-            return np.max(x, axis=(0, 1)) # (H,W,C) menjadi (C,)
-        return np.max(x, axis=(1, 2)) # (N,H,W,C) menjadi (N,C)
+            return np.max(x, axis=(0, 1))
+        return np.max(x, axis=(1, 2))
+
+    def backward(self, dout):
+        x = self._cache['x']
+        single = x.ndim == 3
+        if single:
+            x   = x[np.newaxis]
+            dout = dout[np.newaxis]
+
+        max_vals = x.max(axis=(1, 2), keepdims=True)
+        mask = (x == max_vals).astype(np.float32)
+        mask /= mask.sum(axis=(1, 2), keepdims=True)
+        dx = mask * dout[:, np.newaxis, np.newaxis, :]
+
+        return dx[0] if single else dx
 
 
 class GlobalAvgPooling2DLayer:
+    def __init__(self):
+        self._cache = {}
+
     def forward(self, x):
+        self._cache = {'x_shape': np.asarray(x, dtype=np.float32).shape}
         if x.ndim == 3:
-            return np.mean(x, axis=(0, 1)) # (H,W,C) menjadi (C,)
-        return np.mean(x, axis=(1, 2)) # (N,H,W,C) menjadi (N,C)
+            return np.mean(x, axis=(0, 1))
+        return np.mean(x, axis=(1, 2))
+
+    def backward(self, dout):
+        shape = self._cache['x_shape']
+        single = len(shape) == 3
+
+        if single:
+            H, W, C = shape
+            scale = 1.0 / (H * W)
+            return np.ones((H, W, C), dtype=np.float32) * scale * dout[np.newaxis, np.newaxis, :]
+        else:
+            N, H, W, C = shape
+            scale = 1.0 / (H * W)
+            return np.ones((N, H, W, C), dtype=np.float32) * scale * dout[:, np.newaxis, np.newaxis, :]
 
 
 class FlattenLayer:
+    def __init__(self):
+        self._cache = {}
+
     def forward(self, x):
+        self._cache = {'shape': x.shape}
         if x.ndim == 3:
-            return x.flatten(order="C") # single menjadi (features,)
-        return x.reshape(x.shape[0], -1) # batch  menjadi (N, features)
+            return x.flatten(order="C")
+        return x.reshape(x.shape[0], -1)
+
+    def backward(self, dout):
+        return dout.reshape(self._cache['shape'])
 
 
 class DenseLayer:
     def __init__(self, kernel, bias, activation=None):
-        self.kernel     = np.asarray(kernel, dtype=np.float32)
-        self.bias       = np.asarray(bias,   dtype=np.float32)
+        self.kernel = np.asarray(kernel, dtype=np.float32)
+        self.bias = np.asarray(bias,   dtype=np.float32)
+        self._act_name = activation
         self.activation = get_activation(activation)
+        self._cache = {}
 
     def forward(self, x):
-        out = x @ self.kernel + self.bias
-        return self.activation(out) if self.activation else out
+        self._cache['x'] = x
+        z = x @ self.kernel + self.bias
+        self._cache['z'] = z
+        out = self.activation(z) if self.activation else z
+        self._cache['out'] = out
+        return out
+
+    def backward(self, dout):
+        x = self._cache['x']
+        dz = _act_backward(dout, self._act_name, self._cache['z'], self._cache['out'])
+
+        single = x.ndim == 1
+        if single:
+            x = x[np.newaxis]
+            dz = dz[np.newaxis]
+
+        dx = dz @ self.kernel.T
+        dW = x.T @ dz
+        db = dz.sum(axis=0)
+
+        return (dx[0] if single else dx), dW, db
 
 
 class BatchNormLayer:
@@ -223,7 +427,21 @@ class BatchNormLayer:
         self.mean = np.asarray(moving_mean, dtype=np.float32)
         self.var = np.asarray(moving_var,  dtype=np.float32)
         self.eps = epsilon
+        self._cache = {}
 
     def forward(self, x):
-        x_norm = (x - self.mean) / np.sqrt(self.var + self.eps)
-        return self.gamma * x_norm + self.beta
+        sigma = np.sqrt(self.var + self.eps)
+        x_hat = (x - self.mean) / sigma
+        out = self.gamma * x_hat + self.beta
+        self._cache = {'x_hat': x_hat, 'sigma': sigma}
+        return out
+
+    def backward(self, dout):
+        x_hat = self._cache['x_hat']
+        sigma = self._cache['sigma']
+
+        d_beta = dout.sum(axis=0) if dout.ndim > 1 else dout
+        d_gamma = (dout * x_hat).sum(axis=0) if dout.ndim > 1 else dout * x_hat
+        dx = dout * self.gamma / sigma
+
+        return dx, d_gamma, d_beta
--- src/rnn_lstm/layers.py
+++ src/rnn_lstm/layers.py
@@ -12,29 +12,65 @@ class EmbeddingLayer:
         return self.W.shape[1]
 
     def forward(self, token_id):
+        self._cache_token = int(token_id)
         return self.W[int(token_id)]
 
     def forward_batch(self, token_ids):
-        return self.W[np.asarray(token_ids, dtype=np.int32)]
+        self._cache_tokens = np.asarray(token_ids, dtype=np.int32)
+        return self.W[self._cache_tokens]
 
     def forward_sequence(self, token_ids):
         return self.W[token_ids]
+
+    def backward(self, dout, token_ids=None):
+        if token_ids is None:
+            token_ids = getattr(self, '_cache_tokens',
+                                getattr(self, '_cache_token', None))
+        dW = np.zeros_like(self.W)
+        np.add.at(dW, token_ids, dout)
+        return dW
+
 
 class SimpleRNNCell:
     def __init__(self, kernel, recurrent_kernel, bias):
         self.W_x = np.asarray(kernel,           dtype=np.float32)
         self.W_h = np.asarray(recurrent_kernel, dtype=np.float32)
         self.b = np.asarray(bias,             dtype=np.float32)
+        self._cache = {}
 
     @property
     def units(self):
         return self.W_h.shape[0]
 
     def forward(self, x, h):
-        return np.tanh(x @ self.W_x + h @ self.W_h + self.b)
+        h_new = np.tanh(x @ self.W_x + h @ self.W_h + self.b)
+        self._cache = {'x': x, 'h': h, 'h_new': h_new}
+        return h_new
+
+    def backward(self, dh_new):
+        x, h, h_new = self._cache['x'], self._cache['h'], self._cache['h_new']
+
+        # tanh backward
+        dz = dh_new * (1.0 - h_new ** 2)
+
+        dx = dz @ self.W_x.T
+        dh_prev = dz @ self.W_h.T
+
+        single = x.ndim == 1
+        if single:
+            dW_x = np.outer(x, dz)
+            dW_h = np.outer(h, dz)
+            db = dz
+        else:
+            dW_x = x.T @ dz
+            dW_h = h.T @ dz
+            db = dz.sum(axis=0)
+
+        return dx, dh_prev, dW_x, dW_h, db
 
     def zero_state(self, batch_size=1):
         return np.zeros((batch_size, self.units), dtype=np.float32)
+
 
 class LSTMCell:
     def __init__(self, kernel, recurrent_kernel, bias):
@@ -42,6 +78,7 @@ class LSTMCell:
         self.W_h = np.asarray(recurrent_kernel, dtype=np.float32)
         b = np.asarray(bias, dtype=np.float32)
         self.b = b[0] + b[1] if b.ndim == 2 else b
+        self._cache = {}
 
     @property
     def units(self):
@@ -58,7 +95,54 @@ class LSTMCell:
 
         c_new = f * c + i * g
         h_new = o * np.tanh(c_new)
+
+        self._cache = {
+            'x': x, 'h': h, 'c': c,
+            'i': i, 'f': f, 'g': g, 'o': o,
+            'c_new': c_new, 'h_new': h_new,
+        }
         return h_new, c_new
+
+    def backward(self, dh_new, dc_new):
+        x, h, c_prev = self._cache['x'], self._cache['h'], self._cache['c']
+        i, f, g, o = self._cache['i'], self._cache['f'], self._cache['g'], self._cache['o']
+        c_new = self._cache['c_new']
+
+        tanh_c_new = np.tanh(c_new)
+
+        # h_new = o * tanh(c_new)
+        do = dh_new * tanh_c_new
+        dc_from_h = dh_new * o * (1.0 - tanh_c_new ** 2)
+        dc = dc_new + dc_from_h        
+
+        # c_new = f * c_prev + i * g
+        di      = dc * g
+        df      = dc * c_prev
+        dg      = dc * i
+        dc_prev = dc * f
+
+        # Gate activation backward
+        di_raw = di * i * (1.0 - i)    
+        df_raw = df * f * (1.0 - f)    
+        dg_raw = dg * (1.0 - g ** 2)   
+        do_raw = do * o * (1.0 - o)    
+
+        dgates = np.concatenate([di_raw, df_raw, dg_raw, do_raw], axis=-1)
+
+        dx = dgates @ self.W_x.T
+        dh_prev = dgates @ self.W_h.T
+
+        single = x.ndim == 1
+        if single:
+            dW_x = np.outer(x, dgates)
+            dW_h = np.outer(h, dgates)
+            db = dgates
+        else:
+            dW_x = x.T @ dgates
+            dW_h = h.T @ dgates
+            db = dgates.sum(axis=0)
+
+        return dx, dh_prev, dc_prev, dW_x, dW_h, db
 
     def zero_state(self, batch_size=1):
         z = np.zeros((batch_size, self.units), dtype=np.float32)
@@ -69,14 +153,44 @@ class DenseLayer:
     def __init__(self, kernel, bias, activation=None):
         self.W = np.asarray(kernel, dtype=np.float32)
         self.b = np.asarray(bias,   dtype=np.float32)
-        self.activation = activation
+        self.activation = activation   
+        self._cache = {}
 
     def forward(self, x):
-        out = x @ self.W + self.b
+        self._cache['x'] = x
+        z = x @ self.W + self.b
+        self._cache['z'] = z
         if self.activation == "softmax":
-            out = out - out.max(axis=-1, keepdims=True)
-            e = np.exp(out)
+            z2 = z - z.max(axis=-1, keepdims=True)
+            e  = np.exp(z2)
             out = e / e.sum(axis=-1, keepdims=True)
         elif self.activation == "relu":
-            out = np.maximum(0, out)
+            out = np.maximum(0, z)
+        else:
+            out = z
+        self._cache['out'] = out
         return out
+
+    def backward(self, dout):
+        x   = self._cache['x']
+        z   = self._cache['z']
+        out = self._cache['out']
+
+        if self.activation == "softmax":
+            s = out
+            dz = s * (dout - (dout * s).sum(axis=-1, keepdims=True))
+        elif self.activation == "relu":
+            dz = dout * (z > 0)
+        else:
+            dz = dout
+
+        single = x.ndim == 1
+        if single:
+            x  = x[np.newaxis]
+            dz = dz[np.newaxis]
+
+        dx = dz @ self.W.T
+        dW = x.T @ dz
+        db = dz.sum(axis=0)
+
+        return (dx[0] if single else dx), dW, db
